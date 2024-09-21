1be165ccde4df4ae0b5def34520d6d918c2e1ddd
fidelinacarvajalsparksli <fidelinacarvajalsparksli@hotmail.com>
2024-09-21 03:21:07 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("668c25607d61465bdd39c8ed895bf4750b917ae61371e6295ad3766c41064d02", "668c25607d61465bdd39c8ed895bf4750b917ae61371e6295ad3766c41064d02") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
